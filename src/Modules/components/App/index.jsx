b7d7289b000061851dabeb07ffc84a808e75ffa3
Iannes <yannispyrou@gmail.com>
2019-11-03 14:53:22 +0000

    add store provider

--- src/Modules/components/App/index.jsx
+++ src/Modules/components/App/index.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { HashRouter } from "react-router-dom"
 import CssBaseline from "@material-ui/core/CssBaseline"
+import { StoreProvider } from "../../../Lib/contexts/Store"
 import AppRouter from "../../router"
 // Material ui
 import MaterialThemeProvider from "../../../Lib/contexts/ThemeProvider"
@@ -8,12 +9,14 @@ import AppLayout from "../common/Layout/AppLayout"
 
 const AppContainer = () => (
   <HashRouter>
-    <CssBaseline />
-    <MaterialThemeProvider>
-      <AppLayout>
-        <AppRouter />
-      </AppLayout>
-    </MaterialThemeProvider>
+    <StoreProvider>
+      <CssBaseline />
+      <MaterialThemeProvider>
+        <AppLayout>
+          <AppRouter />
+        </AppLayout>
+      </MaterialThemeProvider>
+    </StoreProvider>
   </HashRouter>
 )
 
