e9c1f56bb72820e7f8a5cb5d8b72805e1dab696a
Iannes <yannispyrou@gmail.com>
2019-11-03 21:41:07 +0000

    styles

--- src/Modules/components/common/Layout/AppLayout/Styles.js
+++ src/Modules/components/common/Layout/AppLayout/Styles.js
@@ -3,5 +3,6 @@ import styled from "styled-components"
 export const Wrapper = styled.main`
   display: flex;
   justify-content: center;
-  align-items: center;
+  max-width: 85%;
+  margin: auto;
 `
--- src/Modules/components/common/Search/Styles.js
+++ src/Modules/components/common/Search/Styles.js
@@ -2,8 +2,7 @@ import { makeStyles } from "@material-ui/core/styles"
 
 export const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
-    height: 250
+    flexGrow: 1
   },
   container: {
     flexGrow: 1,
