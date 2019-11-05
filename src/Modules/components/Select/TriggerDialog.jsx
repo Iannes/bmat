d0f235b987116b22504ead0b98bac3b671c5ca4c
Iannes <yannispyrou@gmail.com>
2019-11-05 20:20:55 +0000

    css for modal title

--- src/Modules/components/Select/TriggerDialog.jsx
+++ src/Modules/components/Select/TriggerDialog.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { makeStyles } from "@material-ui/core/styles"
-import Typography from "@material-ui/core/Typography"
 import IconButton from "@material-ui/core/IconButton"
 import Tooltip from "@material-ui/core/Tooltip"
 import FilterListIcon from "@material-ui/icons/FilterList"
@@ -8,12 +7,11 @@ import { useStoreDispatch } from "../../../Lib/contexts/Store"
 
 const useStyles = makeStyles(() => ({
   icon: {
-    flexGrow: 1,
-    color: "white"
+    flexGrow: 1
   },
   title: {
     flexGrow: 1,
-    color: "white",
+    color: "black",
     marginRight: 20
   }
 }))
