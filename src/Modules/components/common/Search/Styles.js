9a92a04140727afba8158b41fb6061ca78d68a48
Iannes <yannispyrou@gmail.com>
2019-11-05 22:33:19 +0000

    input styles

--- src/Modules/components/common/Search/Styles.js
+++ src/Modules/components/common/Search/Styles.js
@@ -1,31 +1,14 @@
 import { makeStyles } from "@material-ui/core/styles"
 
-export const useStyles = makeStyles(theme => ({
+export const useStyles = makeStyles(() => ({
   root: {
     flexGrow: 1
+  },
+  input: {
+    width: "100%"
   },
   container: {
     flexGrow: 1,
     position: "relative"
-  },
-  paper: {
-    position: "absolute",
-    zIndex: 1,
-    marginTop: theme.spacing(1),
-    left: 0,
-    right: 0
-  },
-  chip: {
-    margin: theme.spacing(0.5, 0.25)
-  },
-  inputRoot: {
-    flexWrap: "wrap"
-  },
-  inputInput: {
-    width: "auto",
-    flexGrow: 1
-  },
-  divider: {
-    height: theme.spacing(2)
   }
 }))
