66bff029773f0529c59e558533fd3a3b24da9603
Iannes <yannispyrou@gmail.com>
2019-11-06 22:29:23 +0000

    add form

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,13 +3,17 @@ import Results from "../../Modules/components/Results/Results"
 import Modal from "../../Modules/components/common/Modal"
 import Banner from "../../Modules/components/common/Banner"
 import Search from "../../Modules/components/common/Search"
+import Form from "../../Modules/components/common/Form"
 
 export const Home = ({ state }) => {
   return (
-    <Banner style={{ width: "100%" }}>
-      <Search />
-      <Results state={state} />
-      <Modal data={state.metaData} />
-    </Banner>
+    <>
+      <Banner style={{ width: "100%" }}>
+        <Search />
+        <Modal data={state.metaData} />
+        <Results state={state} />
+        <Form />
+      </Banner>
+    </>
   )
 }
