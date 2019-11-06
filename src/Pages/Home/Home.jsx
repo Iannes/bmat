420804fdbe91c708e6eeb75effcfc508d5be613f
Iannes <yannispyrou@gmail.com>
2019-11-06 21:15:10 +0000

    Tidy up

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,18 +1,10 @@
 import React from "react"
-import { useStoreState } from "../../Lib/contexts/Store"
-import db from "../../Api/db.csv"
-import metaData from "../../Api/metadata.csv"
-import useFetchData from "../../Modules/hooks/useFetchData"
 import Results from "../../Modules/components/Results/Results"
 import Modal from "../../Modules/components/common/Modal"
 import Banner from "../../Modules/components/common/Banner"
 import Search from "../../Modules/components/common/Search"
 
-const Home = () => {
-  const state = useStoreState()
-  useFetchData(db, "FETCH_DATA")
-  useFetchData(metaData, "FETCH_META_DATA")
-
+export const Home = ({ state }) => {
   return (
     <Banner style={{ width: "100%" }}>
       <Search />
@@ -21,5 +13,3 @@ const Home = () => {
     </Banner>
   )
 }
-
-export default Home
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,27 +3,14 @@ import { useStoreState } from "../../Lib/contexts/Store"
 import db from "../../Api/db.csv"
 import metaData from "../../Api/metadata.csv"
 import useFetchData from "../../Modules/hooks/useFetchData"
-import Table from "../../Modules/components/Table"
-import Modal from "../../Modules/components/common/Modal"
-import Banner from "../../Modules/components/common/Banner"
-import Search from "../../Modules/components/common/Search"
+import { Home } from "./Home"
 
 const HomeContainer = () => {
   const state = useStoreState()
   useFetchData(db, "FETCH_DATA")
   useFetchData(metaData, "FETCH_META_DATA")
 
-  return (
-    <Banner style={{ width: "100%" }}>
-      <Search />
-      {state.searchResults && state.searchResults.length > 0 ? (
-        <Table rows={state.searchResults} />
-      ) : (
-        <p>Search</p>
-      )}
-      <Modal data={state.metaData} />
-    </Banner>
-  )
+  return <Home state={state} />
 }
 
 export default HomeContainer
