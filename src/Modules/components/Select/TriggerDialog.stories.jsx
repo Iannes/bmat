b23198660fcbd9e37e50523ba4f881367640b9db
Iannes <yannispyrou@gmail.com>
2019-11-07 20:58:38 +0000

    csv data breaks storybook

--- src/Modules/components/Select/TriggerDialog.stories.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react"
-import { storiesOf } from "@storybook/react"
-import TriggerDialog from "./TriggerDialog"
-
-storiesOf("TriggerDialog", module).add("TriggerDialog", () => <TriggerDialog />)
--- src/Modules/components/Table/Table.stories.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react"
-import { storiesOf } from "@storybook/react"
-import Template from "./Template"
-
-storiesOf("Template", module).add("Template", () => <Template />)
