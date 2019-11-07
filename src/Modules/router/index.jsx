05e5d7666d5b927ab47fc53d08c1cc83cb0172d7
Iannes <yannispyrou@gmail.com>
2019-11-07 19:53:44 +0000

    cleanup

--- src/Modules/router/index.jsx
+++ src/Modules/router/index.jsx
@@ -4,14 +4,12 @@ import { Route, Switch } from "react-router-dom"
 import Spinner from "../components/common/Spinner"
 
 const Home = React.lazy(() => import("../../Pages/Home"))
-const News = React.lazy(() => import("../../Pages/News"))
 const NotFound = React.lazy(() => import("../../Pages/NotFound"))
 
 const AppRouter = () => (
   <React.Suspense fallback={<Spinner />}>
     <Switch>
       <Route path="/" exact component={() => <Home />} />
-      <Route path="/news" component={() => <News />} />
       <Route component={() => <NotFound />} />
     </Switch>
   </React.Suspense>
--- src/Pages/News/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const News = () => (
-  <section className="News">
-    <h3>News</h3>
-  </section>
-)
-
-export default News
